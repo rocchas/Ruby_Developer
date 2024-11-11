a83b0116ab7ef9dcc765e9ef6bf36526951e3e25
rocchas <carvalho.roccha@gmail.com>
2024-11-11 14:17:49 -0300

    Delete Desafio M1D1.rb

--- Desafio M1D1.rb
+++
@@ -1,7 +0,0 @@
-print "Por favor, digite o seu nome:"
-nome = gets.chomp
-print "Por favor, digite o seu sobrenome:"
-sobrenome = gets.chomp
-print "Por favor, digite a sua idade:"
-idade = gets.chomp.to_i
-puts "Seja bem vindo #{nome} #{sobrenome}! Sua idade é #{idade} anos."
